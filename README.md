4e9b69cd0c7f0d347f2628ffb071d12f080e689a
mitsuru <codeberg@yamamech.sh>
2024-02-20 23:28:34 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Starter Config
+
+if suggestions don't work, first make sure
+you have TypeScript LSP working in your editor
+
+if you do not want typechecking only suggestions
+
+```json
+// tsconfig.json
+"checkJs": false
+```
+
+types are symlinked to:
+/nix/store/jdqwdy9n23rlqpznvnmriym39klqi07a-ags-1.7.7/share/com.github.Aylur.ags/types
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    ags.url = "github:Aylur/ags";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+    systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+      pkgsFor = forEachSystem (system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      });
+
+      devShellFor = system: nixpkgs.lib.genAttrs [ "default" ] (_: nixpkgs.legacyPackages.${system}.mkShell {
+        buildInputs = [
+          pkgsFor.${system}.fish
+          pkgsFor.${system}.typescript
+          pkgsFor.${system}.bun
+          pkgsFor.${system}.libnotify
+          inputs.ags.packages.${system}.agsWithTypes
+        ];
+        nativeBuildInputs = with pkgsFor.${system}; [
+          nixpkgs-fmt
+          nil
+        ];
+        shellHook = ''
+          export GDK_BACKEND=wayland
+        '';
+      });  
+  in {
+    devShells = forEachSystem (system: devShellFor system);
+
+    packages = forEachSystem(system: 
+    let pkgs = pkgsFor.${system}; in {
+      default = (pkgs.callPackage ./. { inherit inputs; }).desktop.script;
+    });
+  };
+}
